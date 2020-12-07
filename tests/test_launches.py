094ff0aa27e26807915368e76cdb6c337d5979df
Igor Verizub <iverizub@raraavis.org>
2020-12-07 14:46:25 +0300

    Added new tests

--- tests/test_launches.py
+++ tests/test_launches.py
@@ -7,17 +7,48 @@ def test_launches_by_id(url, flight_number):
     response = requests.get(url + f'launches/{flight_number}')
     assert response.json()['flight_number'] == flight_number
     assert response.status_code == 200
-    print(response.json()['mission_name'])
 
 
 def test_all_launches(url):
     response = requests.get(url + 'launches')
     assert response.status_code == 200
-    print(response.headers)
 
 
 def test_all_launches_error(url):
     response = requests.get(url + 'launches?flight_number=0')
     assert response.status_code == 200
     assert response.json() == []
-    print(response.json())
+
+
+def test_all_ships(url):
+    response = requests.get(url + 'ships')
+    assert response.status_code == 200
+    assert response.json()[0]['ship_id'] == 'AMERICANCHAMPION'
+
+
+def test_all_ships_roles(url):
+    response = requests.get(url + 'ships')
+    assert response.status_code == 200
+    assert response.json()[0]['roles'][0] == 'Support Ship'
+
+
+def test_get_ship_by_id(url):
+    ship_id = requests.get(url + 'ships').json()[0]['ship_id']
+    response = requests.get(url + f'ships/{ship_id}')
+    assert response.status_code == 200
+    assert response.json()['ship_id'] == ship_id
+
+
+def get_ships_id():
+    ships = requests.get('https://api.spacexdata.com/v3/ships').json()
+    ships_id = []
+    for i in ships:
+        ships_id.append(i['ship_id'])
+    return ships_id
+
+
+@pytest.mark.parametrize("ship_id", get_ships_id())
+def test_get_all_ship_by_id(url, ship_id):
+    response = requests.get(url + f'ships/{ship_id}')
+    assert response.status_code == 200
+    assert response.json()['ship_id'] == ship_id
