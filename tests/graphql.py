303238b812196d4ab13663821d7fa52b7eaaafe7
Igor Verizub <iverizub@raraavis.org>
2020-12-18 13:52:29 +0300

    Graphql post

---
+++ tests/graphql.py
@@ -0,0 +1,9 @@
+import pytest
+import requests
+
+
+def test_launches_by_id(url):
+    data = {'query': '{ country (code: "UA") {name native capital emoji currency languages {code  name}}}'}
+    response = requests.post('https://countries.trevorblades.com/', json=data)
+    assert response.status_code == 200
+    print(response.text)
